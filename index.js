e9b52920bd8e2357c6b7230c40141b36ddc0e9e7
Juliana Fogg <julianafogg@gmail.com>
2020-02-22 22:39:02 -0500

    add insertion function

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 function findMinAndRemove(array){
-    Math.min(...array) 
+    // Math.min(...array) 
+    if (array.length < 2) {
+        return array[0];
+      } else {
+        return Math.min(array.pop(), findMinAndRemove(array))
+      }
 
 }
 
 function insertionSort(array){
-
+    let newMin;
+    let sorted = []
+    while(array.length != 0){
+      newMin = findMinAndRemove(array)
+      sorted.push(newMin)
+    }
+    return sorted;
 }
