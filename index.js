575b749ca2b1b70391dab8fb79e25969b53db35e
Juliana Fogg <julianafogg@gmail.com>
2020-02-22 22:46:39 -0500

    add includes Number function

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-function maxOf(myArray){
-    if (myArray.length < 2) {
-      return myArray[0];
+// function maxOf(myArray){
+//     if (myArray.length < 2) {
+//       return myArray[0];
+//     } else {
+//       return Math.max(myArray.pop(), maxOf(myArray))
+//     }
+//   }
+
+function includesNumber(myArray, num){
+    if (!myArray.length) {
+      return false;
+    } else if (myArray[0] === num){
+      return true;
     } else {
-      return Math.max(myArray.pop(), maxOf(myArray))
+      return includesNumber(myArray.slice(1), num);
     }
-  }
+   }
 
 function findMinAndRemove(array){
     // Math.min(...array) 
@@ -13,7 +23,6 @@ function findMinAndRemove(array){
       } else {
         return Math.min(array.pop(), findMinAndRemove(array))
       }
-
 }
 
 function insertionSort(array){
